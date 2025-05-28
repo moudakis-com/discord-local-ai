6c06f90b9e02d9c9b1f63e8f821062d3d8293ebf
natefoxr <nate@moudakis.com>
2025-05-28 05:34:24 -0700

    Dead code removal

--- homie.py
+++ homie.py
@@ -12,7 +12,6 @@ DISCORD_BOT_TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 
 # Setup Logging
 logger = logging.getLogger(__name__)
-#logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='homieOfAi.log', encoding='utf-8', level=logging.INFO)
 file_handler = logging.FileHandler('homieOfAi.log', encoding='utf-8')
 file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s:%(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'))
 logger.addHandler(file_handler)
